2150755d4895b11815a7d3a9586d5de869806d0b
milton.camachocamach <milton.camachocamach@ucalgary.ca>
2024-09-17 08:52:06 -0600

    enh: work with apple silicon also

--- part3.sh
+++ part3.sh
@@ -51,21 +51,53 @@ datalad save -r -m "add inputs to patient 1"
 # download the image
 datalad containers-add multiply-numbers --call-fmt 'singularity exec -B {{pwd}} {img} {cmd}' --url docker://miltoncamacho/multiply-numbers
 # the --call-fmt allows you to format how the container will be run, in this case, we care about binding the pwd into the container
-# datalad containers-add multiply-numbers --call-fmt 'docker run -v --rm {{pwd}}:/data {img} {cmd}' --url dhub://miltoncamacho/multiply-numbers --update
-# for macs with silicon chips
-# datalad containers-add multiply-numbers --call-fmt 'docker run -v --rm --platform linux/amd64 {{pwd}}:/data {img} {cmd}' --url dhub://miltoncamacho/multiply-numbers --update
+# if using docker:
+#   datalad containers-add multiply-numbers --url dhub://miltoncamacho/multiply-numbers
+# if using mac with silicon chip
+#   we need to add the flag <--platform linux/amd64) inside the docker.py script from the datalad_containers library in my case: /Users/milton/Downloads/heudiconv-new-test/multiecho-proc/env/lib/python3.11/site-packages/datalad_container/adapter/docker.py
+# def cli_run(namespace):
+#     image_id = load(namespace.path, namespace.repo_tag, namespace.config)
+#     prefix = ["docker", "run",
+#               # Add the platform flag here
+#               "--platform", "linux/amd64",  <<<<<<<<<<<<----------------------------# this is the line that matters
+#               # FIXME: The -v/-w settings are convenient for testing, but they
+#               # should be configurable.
+#               "-v", "{}:/tmp".format(os.getcwd()),
+#               "-w", "/tmp",
+#               "--rm",
+#               "--interactive"]
+#     if not on_windows:
+#         # Make it possible for the output files to be added to the
+#         # dataset without the user needing to manually adjust the
+#         # permissions.
+#         prefix.extend(["-u", "{}:{}".format(os.getuid(), os.getgid())])
+
+#     if sys.stdin.isatty():
+#         prefix.append("--tty")
+#     prefix.append(image_id)
+#     cmd = prefix + namespace.cmd
+#     lgr.debug("Running %r", cmd)
+#     sp.check_call(cmd)
 
 # check the containers
 datalad containers-list
 
 # run datalad containers-run 
 cd $study_dir
+# if using singularity
 datalad containers-run \
     --message "Multiplication analysis of patient 1" \
     --container-name multiply-numbers \
     --input "patient_1/inputs/sample_01.txt" \
     --output "patient_1/outputs_multiplication/sample_01_multiplied.txt" \
     "python /app/multiply_numbers.py {inputs} {outputs}"
+# if using docker
+datalad containers-run \
+    --message "Multiplication analysis of patient 1" \
+    --container-name multiply-numbers \
+    --input "patient_1/inputs/sample_01.txt" \
+    --output "patient_1/outputs_multiplication/sample_01_multiplied.txt" \
+    "{inputs} {outputs}"
 
 # modify the values of sample01 for patient 1
 cp $study_dir/patient_1/outputs_multiplication/sample_01_multiplied.txt $study_dir/patient_1/inputs/sample_01.txt
